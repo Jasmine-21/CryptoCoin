e193bafced4b1508c84044672802b66fd02d35c4
Jasmine Singla <55360377+Jasmine-21@users.noreply.github.com>
2020-01-10 22:13:20 +0530

    Cryptocoin

---
+++ src/Heading.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import './Heading.css';
+
+function Heading()
+{
+    return(
+        <div className="display">
+            <center>
+                <h1>
+                    Crypto Wiki
+                </h1>
+            </center>
+            <div className="input-group input group-sm">
+                <input type="text" placeholder="Enter the Crypto Currency name" name="crypto" className="form-control"/>
+                <button className="btn btn-success btn-sm">
+                    Get info about coin
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Heading;
