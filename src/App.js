e22267521efed8a40baee06c85c1eeb44666d8d0
Jasmine Singla <55360377+Jasmine-21@users.noreply.github.com>
2020-01-12 00:19:35 +0530

    cryptocoin

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Heading from './Heading';
 import Body from './Body';
-
+import Content from './Content';
 function App() {
   return (
     <div className="App">
       <Heading/>
       <Body/>
+      <Content/>
     </div>
   );
 }
