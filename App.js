a432d835dcb56d62296c280b27f22c9a52e35a04
Jasmine Singla <55360377+Jasmine-21@users.noreply.github.com>
2020-01-12 00:14:38 +0530

    cryptocoin

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Heading from './Heading';
+import Body from './Body';
+import Content from './Content';
+function App() {
+  return (
+    <div className="App">
+      <Heading/>
+      <Body/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+function Body()
+{
+    return (
+<div>
+    <center>
+        <p>
+            Search for a coin 
+        </p>
+    </center>
+</div>
+    );
+}
+export default Body;
