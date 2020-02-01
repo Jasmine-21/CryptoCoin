b72d04f85a47db2a6f09da6b1c4a76c2fb098876
Jasmine Singla <55360377+Jasmine-21@users.noreply.github.com>
2020-02-01 16:06:14 +0530

    crypto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React ,{Component}from 'react';
-import Form from './Components/Form';
+import React, { Component } from 'react';
+import Header from './Header';
+import DataCoin from "./Components/DataCoin";
+import "./style.css"
+
 class App extends Component {
-  render(){
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
+  state = {
+    coins: []
+  }
+  async componentDidMount() {
+      const  url='https://api.coingecko.com/api/v3/coins/list';
+      const res=await fetch(url);
+      const data=await res.json();
+      console.log(data)
+      // console.log(data.results[0].name.first);
+      this.setState({coins: data});
+  }
+  render() {
+    const { coins } = this.state;
+    console.log(this.state)
+    const coinsList = (coins.length > 0) ? (
+      <DataCoin />
+    ) :
+      (
+        <div>
+          <div className="loading-content"> Loading The Coins List </div>
+        </div>
+      );
+
+    return (
+      <div basename="/Cryptocoin-Wiki">
+        <Header />
+        {coinsList}
+      </div>
+    )
   }
 }
 
